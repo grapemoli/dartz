08b10b52115f2c9ba67405f3e81258fa42016f8b
grapemoli <wambamgara@gmail.com>
2024-04-21 03:44:41 -0400

    client: first display - start screen

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ from coapthon.client.helperclient import HelperClient
 import threading
 import sys
 from PyQt6.QtWidgets import *
+from PyQt6.QtCore import QSize, Qt
 
 
 
@@ -43,13 +44,90 @@ def getDistance ():
 # The GUI.
 ####################################
 class Client (QMainWindow):
-    print ("hello world")
+
+    # Constructor.
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle ("Dartz")
+        self.setMinimumSize (QSize (800, 600))
+
+        # Game State variables.
+        self.players = 0
+        self.distance = 1
+
+        # Because we toggle through multiple windows, we use self.stack to hold
+        # all the "windows," which will be further managed and toggled by display ().
+        self.mainWindow = QWidget ()
+        self.stack = QStackedLayout ()
+        self.mainWindow.setLayout (self.stack)
+        self.setCentralWidget (self.mainWindow)
+
+        # Display #1: Start Screen.
+        self.startScreenWidget = QWidget ()
+        self.startScreenLayout = QVBoxLayout ()
+        self.startScreenWidget.setLayout (self.startScreenLayout)
+
+        # Title.
+        self.title = QLabel ("<font size='4'>Dartz 🎯</font>")
+        self.title.setAlignment (Qt.AlignmentFlag.AlignCenter)
+        self.startScreenLayout.addWidget (self.title)
+
+        # Toggling the game settings: number of players.
+        self.numberOfPlayersLayout = QHBoxLayout ()
+        self.numberOfPlayersLabel = QLabel ("Number of Players")
+        self.numberOfPlayersWidget = QSpinBox ()
+        self.numberOfPlayersWidget.setMinimum (1)
+        self.numberOfPlayersLayout.addWidget (self.numberOfPlayersLabel)
+        self.numberOfPlayersLayout.addWidget (self.numberOfPlayersWidget)
+        self.startScreenLayout.addLayout (self.numberOfPlayersLayout)
+
+        # Toggling the game settings: throwing distance.
+        self.throwingDistanceLayout = QHBoxLayout ()
+        self.throwingDistanceLabel = QLabel ("Throwing Distance")
+        self.throwingDistanceWidget = QDoubleSpinBox ()
+        self.throwingDistanceWidget.setRange (0.5, 4)
+        self.throwingDistanceWidget.setSingleStep (0.5)
+        self.throwingDistanceWidget.setSuffix (" m")
+        self.throwingDistanceLayout.addWidget (self.throwingDistanceLabel)
+        self.throwingDistanceLayout.addWidget (self.throwingDistanceWidget)
+        self.startScreenLayout.addLayout (self.throwingDistanceLayout)
+
+        # Start game button.
+        self.startButton = QPushButton ("Start Game")
+        self.startButton.setCheckable (True)
+        self.startButton.clicked.connect (self.startButton_clicked)
+        self.startScreenLayout.addWidget (self.startButton)
+
+        self.stack.addWidget (self.startScreenWidget)
+
+
+
+
+    # Event Handlers
+    def startButton_clicked (self):
+        # We 'turn off' the button, as there is setup to the CoAP server that
+        # mayy cause a lag time.
+        self.startButton.setText ("Starting game...")
+        self.startButton.setEnabled (False)
+
+        # Set the settings configured by the user.
+        self.players = self.numberOfPlayersWidget.value ()
+        self.distance = self.throwingDistanceWidget.value ()
+        print (self.players)
+        print (self.distance)
+
+        # Show the next layout.
+
+
 
 
 
 ####################################
 # Function: cli
 # The CLI showing of the game interface for the user.
+# The CLI version explicitly shows the steps happening in the GUI,
+# which gets convoluted with GUI elements.
 ####################################
 def cli ():
     game = input("Start Game? (Y/N) ")
@@ -106,11 +184,10 @@ def main ():
     app = QApplication (sys.argv)
 
     window = Client ()
-    window.start ()
     window.show ()
 
     sys.exit (app.exec ())
 
 if __name__ == '__main__':
-    cli ()
+    main ()
 
